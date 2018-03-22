45cd3c0ce1818be35bab92b3266c2cbdc344006d
docluv <chris@love2dev.com>
2018-03-22 06:36:51 -0400

    updated the service worker to clean the caches in version updates

--- public/sw.js
+++ public/sw.js
@@ -30,6 +30,8 @@ const version = "1.01",
 
 self.addEventListener("install", event => {
 
+    self.skipWaiting();
+
     console.log("Installing the service worker!");
 
     caches.open(preCache)
@@ -43,8 +45,25 @@ self.addEventListener("install", event => {
 
 self.addEventListener("activate", event => {
 
-    console.log("Activating the service worker!");
+    event.waitUntil(
 
+        caches.keys().then(cacheNames => {
+          cacheNames.forEach(value => {
+    
+            if (value.indexOf(version) < 0) {
+              caches.delete(value);
+            }
+    
+          });
+    
+          console.log("service worker activated");
+    
+          return;
+    
+        })
+    
+      );
+    
 });
 
 self.addEventListener("fetch", event => {
@@ -62,10 +81,13 @@ self.addEventListener("fetch", event => {
 
                         if (response.ok) {
 
+                            //I have no clue why the chrome extensions requests are passed through the SW
+                            //but I don't like the error messages in the console ;)
                             if (event.request.url.indexOf("chrome-extension") === -1) {
 
                                 let copy = response.clone();
 
+                                //if it was not in the cache it must be added to the dynamic cache
                                 caches.open(dynamicCache)
                                     .then(cache => {
                                         cache.put(event.request, copy);
